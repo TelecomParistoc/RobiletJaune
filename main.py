6de081de97014f8cab657d2d47cfa381882f81f5
superpingu <arnaudbonetti@me.com>
2018-05-14 05:32:19 +0200

    WIP: actions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-import init_and_LED
-
-import motion                           #from libmotors
+import motion                           
+#from libmotors
 from starting_block import add_jack_and_delay, time_elapsed, manage_time_elapsed
-# the robot is "constructed" in structure_Grobot.py
+# the robot is "constructed" in structure.py
 from structure import *
 
 from time import sleep
@@ -11,21 +10,51 @@ from time import sleep
 STARTING_POINT = None #set in init_color()
 STARTING_HEADING = None
 
+PATH_DIR="/home/pi/RobiletJaune/paths"
+
+########################  JACK MANAGEMENT ####################
+
+# delay = 10 = maximum time the robot waits before aborting
+#manage_jack = add_jack_and_delay(robot, 6666)
+
+#init_and_LED.turn_orange_on()
+#gpio.assign_callback_on_gpio_down(jack_pin_bcm, lambda: (manage_jack(False),
+#                                                init_and_LED.turn_green_on()))
+#gpio.assign_callback_on_gpio_up(jack_pin_bcm, lambda: manage_jack(True))
+
+#robot.wait_sequence() # We wait for jack beeing pushed/pulled
 
 ############################ TOP LEVEL ACTION DEFINITION #######################
 # remainder: definition != execution
 
+robot.start_collision_detection(is_obstacle_forwards, is_obstacle_backwards)
+
 def log(message):
     print message
 
+#robot.moveTo(1000,0,-1)
+
+robot.setPosition(203, 1600)
+robot.set_heading(0)
+
+'''robot.add_sequence("main sequence")
+robot.add_parallel(robot.moveTo, [1000, 0, -1])
+robot.wait()
+robot.sequence_done()
+
+robot.start_sequence("main sequence")
+robot.wait_sequence()
+robot.stop()
+'''
+robot.color="green"
+
 
 robot.add_sequence("main_sequence")
-robot.add_parallel(robot.move, [1000])
-robot.wait()
-robot.add_parallel(robot.move, [-1000])   
+robot.load_add_path(PATH_DIR + "/activate_experience.json", False)
+robot.add_parallel(pump, [1], False)
 robot.wait()
 robot.sequence_done()
 
 robot.start_sequence("main_sequence")
 robot.wait_sequence()
-robot.stop() 
+robot.stop()
--- structure.py
+++ structure.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import init_and_LED
 from robot import Robot		#robot is an installed package
 import motordriver
 from AX12 import AX12
@@ -12,7 +13,7 @@ import math
 
 ############################# PARAMETERS #######################################
 
-ROBOT_WIDTH                 = 305       # mm
+ROBOT_WIDTH                 = 250       # mm
 
 LOW_TORQUE                  = 10       # in range [0, 100]
 
@@ -22,15 +23,20 @@ LOW_TORQUE                  = 10       # in range [0, 100]
 #see the output of "gpio readall" on a raspi
 
 
-
+jack_pin_bcm = gpio.gpio_index_of_wpi_pin               (10)
+gpio.set_pin_mode(jack_pin_bcm, gpio.INPUT)
 #gpio.OUTPUT -> easier to test with hand (gpio write 5 0 ou 1)
 # ie we don't test with the real jack, we just simulate it
 #with the real jack, it must be gpio.INPUT )
 
 ##################     PIN INITIALIZATION           ############################
 
+pump_pin_bcm          = gpio.gpio_index_of_wpi_pin    (1)
+gpio.set_pull_up_down(  pump_pin_bcm,           gpio.PULL_UP)
+gpio.set_pin_mode(      pump_pin_bcm,           gpio.OUTPUT)
+
 front_sensors_pin_list_bcm = list(map(gpio.gpio_index_of_wpi_pin,   [3, 4]))
-rear_sensors_pin_list_bcm = list(map(gpio.gpio_index_of_wpi_pin,    [2, 0]))
+rear_sensors_pin_list_bcm = [] #list(map(gpio.gpio_index_of_wpi_pin,    [2, 0]))
 
 for pin in front_sensors_pin_list_bcm + rear_sensors_pin_list_bcm:
     gpio.set_pull_up_down(pin, gpio.PULL_DOWN)
@@ -39,7 +45,7 @@ for pin in front_sensors_pin_list_bcm + rear_sensors_pin_list_bcm:
 ##################     CONSTRUCTION OF THE ROBOT    ############################
 
 #contains the (name, id) of the used AX12
-AX12_list = []
+AX12_list = [("atom_pusher", 129)]
 
 robot = Robot()
 for name, id in AX12_list:
@@ -56,7 +62,7 @@ def grobot_time_elapsed():
     for name, _ in AX12_list:
         getattr(robot, name).turn(0)
 	getattr(robot, name).set_torque(0)
-    gpio.digital_write(shaker_pin_bcm, 0)
+    gpio.digital_write(pump_pin_bcm, 0)
     robot.emergency_stop()
 
 def is_obstacle_forwards():
@@ -71,4 +77,17 @@ def is_obstacle_backwards():
             return True
     return False
 
+def pump(duration=2.):
+    #duration is in seconds
+    print("Pump start")
+    #we don't use pwm
+    gpio.digital_write(pump_pin_bcm, 1)
+    sleep(duration)
+    gpio.digital_write(pump_pin_bcm, 0)
+    print("pump stop")
 
+def push_atom(active = True):
+    if(active):
+        robot.atom_pusher.move(100)
+    else:
+        robot.atom_pusher.move(50)
